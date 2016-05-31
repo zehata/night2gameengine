ea113fa74c8690007ba8aa10e3e558a378d6529e
zehata <zehata@gmail.com>
2016-05-31 22:34:43 +0800

    removed everything

--- The Time Traveller/Scene/OLD demo/objects.js
+++
@@ -1,4 +0,0 @@
-var o003006 = '{"type":"button", "goto":null}';
-var o005006 = '{"type":"doorleft", "goto":null}';
-var o006006 = '{"type":"bothdoors", "leftdoor":"o005006","rightdoor":"o007006"}';
-var o007006 = '{"type":"doorright", "goto":null}';
